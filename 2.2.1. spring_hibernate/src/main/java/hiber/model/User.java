7055dd31605eb9ae3e11163a4463840e337b886a
Sergio19888 <9263924@mail.ru>
2024-07-18 16:49:55 +0300

    ok

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
+   @OneToOne (optional = false, cascade = CascadeType.ALL, mappedBy = "user")
+   private Car car;
+
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
@@ -20,12 +23,14 @@ public class User {
    private String email;
 
    public User() {}
-   
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
    }
+
+
 
    public Long getId() {
       return id;
@@ -57,5 +62,24 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar(){
+      return car;
+   }
+
+   public Car setCar(Car car){
+      this.car = car;
+      return car;
+   }
+
+   @Override
+   public String toString() {
+      return "User {" +
+              "id="  + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              '}';
    }
 }
